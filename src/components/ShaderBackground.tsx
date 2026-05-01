62a1162edea3891036a3abc7a841b391515e5b93
Sung Won Chung <sungwonchung3@gmail.com>
2026-05-01 14:59:37 -0700

    fix color

--- src/components/ShaderBackground.tsx
+++ src/components/ShaderBackground.tsx
@@ -3,8 +3,8 @@ import './ShaderBackground.css'
 
 export function ShaderBackground() {
   return (
-    <Shader className="shader-background">
-      <SolidColor color="#1673FF" />
+    <Shader className="shader-background" colorSpace="srgb">
+      <SolidColor color="#1573FF" />
       <DotGrid color="#b3ccff" density={75} dotSize={0.18} opacity={0.3} />
     </Shader>
   )
